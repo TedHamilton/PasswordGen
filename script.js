0aba47dc7b9afc3a3b1ebbb98af880a21f9007e5
TedHamilton <tedwarehamilton@gmail.com>
2022-07-14 22:07:14 -0400

    adding js variables

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-  
+const resultEl = document.getElementById('password');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const btnEl = document.getElementById('button');
+
+const randomFunc = {
+    lower
+}
